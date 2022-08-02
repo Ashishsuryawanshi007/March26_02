14b3944d46dd9415cbb12d175774978983d37d2f
Nilesh <Nilesh@DESKTOP-459N96N>
2022-08-02 20:39:50 +0530

    [Ashish]:1st changes in a project

---
+++ src/Sample1/demo1.java
@@ -0,0 +1,12 @@
+package Sample1;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		//Uid- Ashishsuryawanshi007
+		//PWD-Ashishsuryawanshi@1991 / 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
+		//URL-
+	}
+}
